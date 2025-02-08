596021a1cc8964af370ebc673b874869e16d3d6c
Nadezhda Kulemzina <Nadezhda_Kulemzina@epam.com>
2025-02-08 19:34:22 +0300

    hw07-solid Эмулятор банкомата

---
+++ hw07-solid/build.gradle.kts
@@ -0,0 +1,10 @@
+dependencies {
+    implementation ("ch.qos.logback:logback-classic")
+
+    testImplementation ("org.junit.jupiter:junit-jupiter-api")
+    testImplementation ("org.junit.jupiter:junit-jupiter-engine")
+    testImplementation ("org.assertj:assertj-core")
+    testImplementation ("org.mockito:mockito-core")
+    testImplementation ("org.mockito:mockito-junit-jupiter")
+    implementation("org.projectlombok:lombok:1.18.24")
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,7 @@ include("hw03-annotations")
 include("hw04-gc")
 include("hw05-autoLogging")
 include("hw06-structuralPatterns")
+include("hw07-solid")
 
 pluginManagement {
     val jgitver: String by settings
